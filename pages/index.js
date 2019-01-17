f6d14a8221ce4a2e87548fb7c90e9b29707bd175
Daniel Park <dpeace926@gmail.com>
2019-01-17 14:16:15 -0500

    initialize nextjs

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+import Link from "next/link";
+
+class index extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>Hello world</div>;
+  }
+}
+
+export default index;
